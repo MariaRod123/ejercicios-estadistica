2b8b21d017a6a0e9a1eeb5912ab2c85cbc2ec2c2
María R <72228855+MariaRod123@users.noreply.github.com>
2023-04-14 01:35:53 -0300

    Update and rename ejercicio7.py to ejercicio7-8.py

--- ejercicio7-8.py
+++ ejercicio7-8.py
@@ -1,3 +1,5 @@
+import statistics
+
 lluvia_r3 = [2.1,
              0,
              0,
@@ -23,6 +25,15 @@ lluvia_r3 = [2.1,
 # Acumulados de lluvia durante el período dado
 acumulados_lluvia = sum(lluvia_r3)
 print("Total lluvias acumuladas : ", round(acumulados_lluvia, 1))
+
+
+# Mediana
+mediana = statistics.median(lluvia_r3)
+print("La mediana es:", mediana)
+
+# Acumulado medio
+acum_medio = acumulados_lluvia / len(lluvia_r3)
+print("Acumulado medio es:", round(acum_medio, 1), "mm")
 
 #Cantidad de dias con registro de 0 mm
 cant_dias_secos = lluvia_r3.count(0)
